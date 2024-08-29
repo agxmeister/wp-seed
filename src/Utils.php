b1e7121ad6721c3e66623de1c7b81fdd15c569c5
agxmeister <agxmeister@gmail.com>
2024-08-29 19:24:32 +0300

    While getting different paths, do not rely on a type of file structure item key to build a full path.

--- src/Utils.php
+++ src/Utils.php
@@ -6,22 +6,19 @@ class Utils
 {
     static public function getDifferentPaths($pathA, $pathB): array
     {
-        if (is_array($pathA)) {
-            $result = [];
-            foreach ($pathA as $subDirectoryA => $subPathA) {
-                $result = [
-                    ...$result,
-                    ...array_map(
-                        fn($path) => is_string($subDirectoryA) ? $subDirectoryA . '/' . $path : $path,
-                        self::getDifferentPaths($subPathA, $pathB[$subDirectoryA] ?? null),
-                    ),
-                ];
-            }
-            return $result;
+        if (is_string($pathA)) {
+            return $pathA === $pathB ? [] : [$pathA];
         }
-        if ($pathA === $pathB) {
-            return [];
+        $result = [];
+        foreach ($pathA as $subDirectoryA => $subPathA) {
+            $result = [
+                ...$result,
+                ...array_map(
+                    fn($path) => is_string($subPathA) ? $path : $subDirectoryA . '/' . $path,
+                    self::getDifferentPaths($subPathA, $pathB[$subDirectoryA] ?? null),
+                ),
+            ];
         }
-        return [$pathA];
+        return $result;
     }
 }
