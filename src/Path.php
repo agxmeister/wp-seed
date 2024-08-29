776e5edcd7ce12d71c99f27e8d653ba9558b4131
agxmeister <agxmeister@gmail.com>
2024-08-29 19:33:31 +0300

    Add the utility for reading a file structure of a given path on a file system.

---
+++ src/Path.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Seed;
+
+readonly class Path
+{
+    public function getStructure($path): array|string
+    {
+        if (is_file($path)) {
+            return basename($path);
+        }
+        $result = [];
+        $handler = opendir($path);
+        while (($entry = readdir($handler)) !== false) {
+            if ($entry === "." || $entry === "..") {
+                continue;
+            }
+            $result[$entry] = $this->getStructure($path . DIRECTORY_SEPARATOR . $entry);
+        }
+        return $result;
+    }
+}
