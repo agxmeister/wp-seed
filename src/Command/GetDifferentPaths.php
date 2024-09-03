d72d55279e152b8c8837782bc5093f4ef5345fa5
agxmeister <agxmeister@gmail.com>
2024-09-03 20:14:28 +0300

    Add a logger to CLI command handlers.

--- src/Command/GetDifferentPaths.php
+++ src/Command/GetDifferentPaths.php
@@ -3,11 +3,16 @@
 namespace Seed\Command;
 
 use Minicli\Command\CommandCall;
+use Monolog\Logger;
 
 readonly class GetDifferentPaths
 {
+    public function __construct(private Logger $logger)
+    {
+    }
+
     public function __invoke(CommandCall $input): void
     {
-        var_dump($input);
+        $this->logger->debug("Getting different paths", [$input]);
     }
 }
--- src/Command/GetStructure.php
+++ src/Command/GetStructure.php
@@ -3,18 +3,20 @@
 namespace Seed\Command;
 
 use Minicli\Command\CommandCall;
+use Monolog\Logger;
 use Seed\Path;
 
 readonly class GetStructure
 {
-    public function __construct(private Path $path)
+    public function __construct(private Logger $logger, private Path $path)
     {
     }
 
     public function __invoke(CommandCall $input): void
     {
+        $this->logger->debug("Getting structure", [$input]);
         [ , , $pathArg] = $input->args;
         $structure = $this->path->getStructure($pathArg);
-        var_dump($structure);
+        $this->logger->debug("Structure is ", [$structure]);
     }
 }
