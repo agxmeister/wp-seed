3b5c84b0a20c4cca2ef65a29bccdb7746b2bd4da
agxmeister <agxmeister@gmail.com>
2024-09-04 18:56:30 +0300

    Move configuring the DI container to a separate function.

---
+++ app/dependencies.php
@@ -0,0 +1,19 @@
+<?php
+
+use DI\ContainerBuilder;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
+use Monolog\Logger;
+
+return function (ContainerBuilder $containerBuilder)
+{
+    $containerBuilder->addDefinitions([
+        Logger::class => DI\factory(function () {
+            $logger = new Logger('log');
+            $fileHandler = new StreamHandler('./main.log', Level::Debug);
+            $fileHandler->setFormatter(new LineFormatter());
+            return $logger->pushHandler($fileHandler);
+        }),
+    ]);
+};
