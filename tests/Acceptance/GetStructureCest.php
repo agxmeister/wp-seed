8afce5d0c83730fd9f4eb50f16c8bcbd7d5f3211
agxmeister <agxmeister@gmail.com>
2024-09-07 21:48:15 +0300

    Add the acceptance tests suite.

---
+++ tests/Acceptance/GetStructureCest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class GetStructureCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
