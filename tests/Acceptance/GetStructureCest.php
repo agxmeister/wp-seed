eb309a60fe9c97c0525c9faa66f86f6fce3d5bcb
agxmeister <agxmeister@gmail.com>
2024-09-10 20:00:29 +0300

    For acceptance tests, move out test data from the code.

--- tests/Acceptance/GetStructureCest.php
+++ tests/Acceptance/GetStructureCest.php
@@ -2,19 +2,44 @@
 
 namespace Tests\Acceptance;
 
+use Codeception\Attribute\Examples;
+use Codeception\Example;
 use Tests\Support\AcceptanceTester;
 
 class GetStructureCest
 {
-    public function _before(AcceptanceTester $I)
+    public function _before(AcceptanceTester $I): void
     {
+        $I->runShellCommand("rm -rf /tmp/*");
     }
 
-    public function tryToTest(AcceptanceTester $I): void
+    #[Examples(
+        [
+            [
+                '/tmp/test',
+            ],
+            [
+                'test' => [],
+            ],
+        ], [
+            [
+                '/tmp/test1',
+                '/tmp/test2',
+            ],
+            [
+                'test1' => [],
+                'test2' => [],
+            ],
+        ],
+    )]
+    public function tryToTest(AcceptanceTester $I, Example $example): void
     {
-        $I->runShellCommand("mkdir -p /tmp/test");
+        [$paths, $expected] = $example;
+        foreach ($paths as $path) {
+            $I->runShellCommand("mkdir -p '$path'");
+        }
         $I->runShellCommand("./bin/seed get-structure /tmp");
         $structure = json_decode($I->grabShellOutput(), true);
-        $I->assertEquals(['test' => []], $structure);
+        $I->assertEquals($expected, $structure);
     }
 }
