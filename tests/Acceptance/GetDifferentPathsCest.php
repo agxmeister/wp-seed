4d9dd14e7cd023e8fa6816e7527704f4edf88281
agxmeister <agxmeister@gmail.com>
2024-09-18 20:31:33 +0300

    Add an acceptance test for the get-different-paths command.

---
+++ tests/Acceptance/GetDifferentPathsCest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Acceptance;
+
+use Codeception\Attribute\Examples;
+use Codeception\Example;
+use Tests\Support\AcceptanceTester;
+
+class GetDifferentPathsCest
+{
+    #[Examples(
+        [
+            [
+                'a/d/f',
+            ], [
+                'b/d/',
+            ], [
+                'd/f',
+            ],
+        ], [
+            [
+                'a/d/',
+            ], [
+                'b/d/f',
+            ], [
+            ],
+        ],
+    )]
+    public function tryToTest(AcceptanceTester $I, Example $example): void
+    {
+        [$pathsA, $pathsB, $expected] = $example;
+        $I->createFileStructure($pathsA);
+        $I->createFileStructure($pathsB);
+        $I->runShellCommand("./bin/seed get-different-paths /tmp/a /tmp/b");
+        $structure = json_decode($I->grabShellOutput(), true);
+        $I->assertEquals($expected, $structure);
+    }
+}
