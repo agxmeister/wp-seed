2dc7e5cd25dfdda599af1813bffebc102afbc0e8
agxmeister <agxmeister@gmail.com>
2024-09-24 21:57:23 +0300

    Add the acceptance test to get a specific version of WordPress core.

--- tests/Acceptance/GetCoreCest.php
+++ tests/Acceptance/GetCoreCest.php
@@ -6,11 +6,19 @@ use Tests\Support\AcceptanceTester;
 
 class GetCoreCest
 {
-    public function tryToTest(AcceptanceTester $I): void
+    public function getLatestTest(AcceptanceTester $I): void
     {
         $I->runShellCommand("./bin/seed get-core --filename=latest.zip");
         $I->seeResultCodeIs(0);
         $basePath = $I->getBasePath();
         $I->runShellCommand("ls -l $basePath/latest.zip");
+    }
+
+    public function getVersionTest(AcceptanceTester $I): void
+    {
+        $I->runShellCommand("./bin/seed get-core --version=5.9.10 --filename=wordpress-5.9.10.zip");
+        $I->seeResultCodeIs(0);
+        $basePath = $I->getBasePath();
+        $I->runShellCommand("ls -l $basePath/wordpress-5.9.10.zip");
     }
 }
