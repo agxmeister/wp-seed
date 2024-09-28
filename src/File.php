fde6fd22b76d404fda6aaf2b66f02c1b5d5794ef
agxmeister <agxmeister@gmail.com>
2024-09-28 20:02:36 +0300

    Instead of specific implementations, use the interface to inject a downloader into the file utility.

--- src/File.php
+++ src/File.php
@@ -4,14 +4,14 @@ namespace Seed;
 
 readonly class File
 {
-    public function __construct(private Http $http, private Storage $storage)
+    public function __construct(private Downloader $downloader, private Storage $storage)
     {
     }
 
     public function getByUrl(string $url, string $fileName): string
     {
         $destinationPath = $this->storage->getPath($fileName);
-        $this->http->download($url, $destinationPath);
+        $this->downloader->download($url, $destinationPath);
         return $destinationPath;
     }
 }
