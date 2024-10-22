749b4e20ced53619d5c668579c8f1752a0f312ad
agxmeister <agxmeister@gmail.com>
2024-10-22 16:42:46 +0300

    While getting the WordPress core package, return the path to the destination package file.

--- src/Package.php
+++ src/Package.php
@@ -14,11 +14,12 @@ readonly class Package
     {
     }
 
-    public function getCore(string $version = null, string $filename = null, PackageType $type = null): void
+    public function getCore(string $version = null, string $filename = null, PackageType $type = null): string
     {
         $url = $this->catalog->getCorePackageUrl($version, $type);
         $destinationPath = $this->storage->getPath($filename ?? basename(parse_url($url, PHP_URL_PATH)));
         $this->file->getByUrl($url, $destinationPath);
+        return $destinationPath;
     }
 
     public function extract(string $sourcePath, string $destinationPath, $type = null): void
